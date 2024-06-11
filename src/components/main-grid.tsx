8604713abe640a0ecfb105d70d64cf89e86c15f0
richardkyk <28803078+richardkyk@users.noreply.github.com>
2024-06-11 10:36:53 -0700

    feat: handle font size

--- src/components/main-grid.tsx
+++ src/components/main-grid.tsx
@@ -3,14 +3,37 @@
 import { useState } from "react";
 
 export function MainGrid() {
-  const [columns, setColumns] = useState(20);
+  const [mainFontSize, setMainFontSize] = useState(16);
+  const [secondaryFontSize, setSecondaryFontSize] = useState(10);
+
+  const [columns, setColumns] = useState(16);
   const [rows, setRows] = useState(10);
 
-  const [columnGap, setColumnGap] = useState(5);
-  const [rowGap, setRowGap] = useState(5);
+  const [columnGap, setColumnGap] = useState(10);
+  const [rowGap, setRowGap] = useState(20);
 
   return (
     <div className="flex flex-col gap-4">
+      <div className="flex gap-4">
+        <div className="flex gap-2">
+          <span>Main Font Size</span>
+          <input
+            type="number"
+            value={mainFontSize}
+            onChange={(e) => setMainFontSize(Number(e.target.value))}
+            className="max-w-12 border pl-2"
+          />
+        </div>
+        <div className="flex gap-2">
+          <span>Secondary Font Size</span>
+          <input
+            type="number"
+            value={secondaryFontSize}
+            onChange={(e) => setSecondaryFontSize(Number(e.target.value))}
+            className="max-w-12 border pl-2"
+          />
+        </div>
+      </div>
       <div className="flex gap-4">
         <div className="flex gap-2">
           <span>Columns</span>
@@ -22,20 +45,20 @@ export function MainGrid() {
           />
         </div>
         <div className="flex gap-2">
-          <span>Rows</span>
-          <input
-            type="number"
-            value={rows}
-            onChange={(e) => setRows(Number(e.target.value))}
-            className="max-w-12 border pl-2"
-          />
-        </div>
-        <div className="flex gap-2">
           <span>Column Gap</span>
           <input
             type="number"
             value={columnGap}
             onChange={(e) => setColumnGap(Number(e.target.value))}
+            className="max-w-12 border pl-2"
+          />
+        </div>
+        <div className="flex gap-2">
+          <span>Rows</span>
+          <input
+            type="number"
+            value={rows}
+            onChange={(e) => setRows(Number(e.target.value))}
             className="max-w-12 border pl-2"
           />
         </div>
@@ -71,12 +94,27 @@ export function MainGrid() {
               {Array(columns)
                 .fill(0)
                 .map((_, j) => (
-                  <div
-                    id={`${i}-${j}`}
-                    key={j}
-                    className={`flex size-8 items-center justify-center border`}
-                    contentEditable
-                  ></div>
+                  <div id={`${i}-${j}`} key={j} className="flex flex-col">
+                    <div
+                      contentEditable
+                      className="flex h-6 w-8 items-center justify-center border"
+                      style={{
+                        fontSize: `${secondaryFontSize}px`,
+                        width: `${mainFontSize * 2}px`,
+                        height: `${secondaryFontSize * 2}px`,
+                      }}
+                    ></div>
+
+                    <div
+                      contentEditable
+                      className="flex items-center justify-center border"
+                      style={{
+                        fontSize: `${mainFontSize}px`,
+                        width: `${mainFontSize * 2}px`,
+                        height: `${mainFontSize * 2}px`,
+                      }}
+                    ></div>
+                  </div>
                 ))}
             </div>
           ))}
